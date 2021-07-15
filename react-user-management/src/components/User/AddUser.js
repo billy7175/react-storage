29a8283d9942db98ade91c67b47cb03f2f8f078a
unknown <whgus439@gmail.com>
2021-07-15 14:44:50 +0900

    Create Input Handlers

--- react-user-management/src/components/User/AddUser.js
+++ react-user-management/src/components/User/AddUser.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandleer = (event) => {
     event.preventDefault();
   };
 
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+    console.log(event.target.value);
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandleer}>
-        <label class="">Username</label>
-        <input id="username" type="text" />
+        <label>Username</label>
+        <input value={enteredUserName} onChange={userNameChangeHandler} id="username" type="text" />
         <label htmlFor="age">Age(Years)</label>
-        <input type="submit" type="number" />
+        <input value={enteredAge} onChange={ageChangeHandler} type="number" type="number" />
         <button>Add User</button>
       </form>
     </Card>
