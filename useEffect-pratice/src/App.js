b78260e0c287e49db326cc7aa95b3754f6a30c29
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-07-18 21:30:22 +0900

    useEffect when componentDidmount

--- useEffect-pratice/src/App.js
+++ useEffect-pratice/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,24 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  
+  
+  useEffect(() => {
+    const storedIsLoggedInInformation = localStorage.getItem('isLoggedIn');
+    if(storedIsLoggedInInformation){
+      setIsLoggedIn(true);
+    }
+  },[]);
+  
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn','1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
@@ -21,6 +32,7 @@ function App() {
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
+        <div>123</div>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
