1a5d7bbfb697c90c5d6123b4d6bd03d348d358fc
unknown <whgus439@gmail.com>
2021-07-22 16:32:59 +0900

    Create AuthContext.Provider

--- useEffect-pratice/src/App.js
+++ useEffect-pratice/src/App.js
@@ -1,41 +1,39 @@
-import React, { useState, useEffect } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import React, { useState, useEffect } from "react";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  
-  
   useEffect(() => {
-    const storedIsLoggedInInformation = localStorage.getItem('isLoggedIn');
-    if(storedIsLoggedInInformation){
+    const storedIsLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedIsLoggedInInformation) {
       setIsLoggedIn(true);
     }
-  },[]);
-  
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn','1');
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+      <AuthContext.Provider>
+        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
   );
 }
 
