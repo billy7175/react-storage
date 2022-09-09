3b9bc159dbf0155dab24185e8ec45c7ae506750e
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-09-10 04:11:31 +0900

    add context api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import { Fragment } from 'react';
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -16,13 +16,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHanlder}></Cart>}
       <Header onShowCart={showCartHandler} onClose={hideCartHanlder}></Header>
       <main>
         <Meals></Meals>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
